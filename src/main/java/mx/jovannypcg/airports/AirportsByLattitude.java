51170c48f4fedda4bc53cea3763abf363964a46e
Jovanny Cruz <jovannypcg@yahoo.com>
2018-09-28 12:30:56 -0500

    Add problem about airports by lattitude

---
+++ src/main/java/mx/jovannypcg/airports/AirportsByLattitude.java
@@ -0,0 +1,7 @@
+package mx.jovannypcg.airports;
+
+public class AirportsByLattitude {
+    public static void main(String[] args) {
+        System.out.println("Running the airports by lattitude problem...");
+    }
+}
